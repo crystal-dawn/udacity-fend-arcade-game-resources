ce57b5f2722ed2c9c02ec05498b39f8b9f63e76c
Crystal Yungwirth <CrystalYungwirth@gmail.com>
2019-02-10 18:16:46 -0500

    Feat: Criteria card

--- js/app.js
+++ js/app.js
@@ -17,22 +17,30 @@ const mainElement = () => {
 }
 
 /**
- * @description loop through array of criterias creating each section
- * @todo remove empty cards
+ * @description Create one criteria card for each item from the rubric
  */
 function criteriaCard() {
-  for (i = 0; i < criteriasList.length; i++) {
-    const criterias = criteriasList.map((criteria, index) => {
-      document.querySelector('main')
-        .insertAdjacentHTML('afterbegin',
-          `<section class="criteria">
-          <h2 class="criteria-header" id="criteria${index}">${criteria}</h2>
-        </section>`);
+  //create a Set for criterias. Sets automatically don't add duplicate values
+  //without sending an error. This creates one list with a single entry for
+  //any criteria listed in resources
+  let criteriaSet = new Set();
+  resources.forEach(criteria => criteriaSet.add(criteria.criteria));
 
-      document.querySelectorAll('span')[1].insertAdjacentHTML('afterbegin',
-        `<a class="dropdown-content-link" href="#criteria${index}">${criteria}</a>`)
-    })
-  }
+  // create an array so that an index can be established.
+  let criteriaArray = [...criteriaSet.values()];
+
+  //one card for each criteria from the array
+  criteriaArray.forEach((criteria, criteriaIndex) => {
+    document.querySelector('nav')
+      .insertAdjacentHTML('afterend',
+        `<section class="criteria">
+        <h2 class="criteria-header" id="criteria${criteriaIndex}">${criteria}</h2>
+      </section>`);
+
+    //populate Rubric criteria dropdown nav
+    document.querySelectorAll('span')[1].insertAdjacentHTML('afterbegin',
+      `<a class="dropdown-content-link" href="#criteria${criteriaIndex}">${criteria}</a>`)
+  })
 }
 
 document.body.onload = loadPage();
