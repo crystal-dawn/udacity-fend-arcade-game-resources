7c2f31dcb6b798c7c7ed403e219fef79b6821caa
Crystal Yungwirth <CrystalYungwirth@gmail.com>
2019-02-10 18:08:10 -0500

    Feat: Custom resources

--- js/resources.js
+++ js/resources.js
@@ -1,25 +1,182 @@
-const resources = [{
-  requirement: "requirement",
-  type: "type",
-  page: "page",
-  url: "url",
-  sharer: "sharer"
-}, {
-  requirement: "requirement",
-  type: "type",
-  page: "page",
-  url: "url",
-  sharer: "sharer"
-}, {
-  requirement: "requirement",
-  type: "type",
-  page: "page",
-  url: "url",
-  sharer: "sharer"
-}, {
-  requirement: "requirement",
-  type: "type",
-  page: "page",
-  url: "url",
-  sharer: "sharer"
-}]
+const resources = new Map();
+
+resources.set("Eloquent JavaScript Chapter 6: The Secret Life of Objects", {
+    url: "https://eloquentjavascript.net/06_object.html",
+    criteria: "Object Oriented Code",
+    type: "Book",
+    sharer: "Matt Cool"
+  })
+  .set("Modals in Pure ES6 JavaScript", {
+    url: "https://lowrey.me/modals-in-pure-es6-javascript/",
+    criteria: "Player Wins",
+    type: "Article/Blog",
+    sharer: "James Theobald"
+  })
+  .set("HTML5 Canvas Game: 2D Collision Detection", {
+    url: "http://blog.sklambert.com/html5-canvas-game-2d-collision-detection/",
+    criteria: "Collision",
+    type: "Text Tutorial",
+    sharer: "jamesperrin-FEND"
+  })
+  .set("Adding collision detection to images drawn on canvas", {
+    url: "https://stackoverflow.com/questions/13916966/adding-collision-detection-to-images-drawn-on-canvas",
+    criteria: "Collision",
+    type: 'Question/Answer',
+    sharer: "jamesperrin-FEND"
+  })
+  .set("CanvasRenderingContext2D.drawImage()", {
+    url: "https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage",
+    criteria: "Player/Vehicle Movement",
+    type: "Documentation",
+    sharer: "James Theobald"
+  })
+  .set("Collision detection", {
+    url: "https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Collision_detection",
+    criteria: "Player/Vehicle Movement",
+    type: "Text Tutorial",
+    sharer: "Tony Boswell [FEND]"
+  })
+  .set("2D collision detection", {
+    url: "https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection",
+    criteria: "Collision",
+    type: "Text Tutorial",
+    sharer: "Ryan B[FEND]"
+  })
+  .set("Speed and Velocity", {
+    url: "https://www.physicsclassroom.com/class/1DKin/Lesson-1/Speed-and-Velocity",
+    criteria: "Player/Vehicle Movement",
+    type: "Text Tutorial",
+    sharer: "Iip"
+  })
+  .set("nodemon", {
+    url: "https://nodemon.io/",
+    criteria: "Extras",
+    type: "Utility",
+    sharer: "Kaisma"
+  })
+  .set("GAME ART GIVEAWAY #7 – TOP DOWN VEHICLES SPRITES PACK", {
+    url: "http://unluckystudio.com/game-art-giveaway-7-top-down-vehicles-sprites-pack/",
+    criteria: "Extras",
+    type: "Design",
+    sharer: "leesav"
+  })
+  .set("Beach Background Clip Art", {
+    url: "http://www.clker.com/search/beach+background/1",
+    criteria: "Extras",
+    type: "Design",
+    sharer: "leesav"
+  })
+  .set("Millions of Graphic Resources For Free Download", {
+    url: "https://pngtree.com/",
+    criteria: "Extras",
+    type: "Design",
+    sharer: "leesav"
+  })
+  .set("this", {
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this",
+    criteria: "Object Oriented Code - Ryan Waite Recommendations",
+    type: "Documentation",
+    sharer: "RyanWaite.ProjectCoach [FEND, MWS]"
+  })
+  .set("Working with objects", {
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects",
+    criteria: "Object Oriented Code - Ryan Waite Recommendations",
+    type: "Documentation",
+    sharer: "RyanWaite.ProjectCoach [FEND, MWS]"
+  })
+  .set("Primitive", {
+    url: "https://developer.mozilla.org/en-US/docs/Glossary/Primitive",
+    criteria: "Object Oriented Code - Ryan Waite Recommendations",
+    type: "Documentation",
+    sharer: "RyanWaite.ProjectCoach [FEND, MWS]"
+  })
+  .set("Inheritance in JavaScript", {
+    url: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Inheritance",
+    criteria: "Object Oriented Code - Ryan Waite Recommendations",
+    type: "Documentation",
+    sharer: "RyanWaite.ProjectCoach [FEND, MWS]"
+  })
+  .set("Details of the object model", {
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Details_of_the_Object_Model",
+    criteria: "Object Oriented Code - Ryan Waite Recommendations",
+    type: "Documentation",
+    sharer: "RyanWaite.ProjectCoach [FEND, MWS]"
+  })
+  .set("How to Start Using JavaScript Promises | With Examples", {
+    url: "https://youtu.be/cGuNTjfZCm8",
+    criteria: "Object Oriented Code",
+    type: "Video Tutorial",
+    sharer: "Donald Maness"
+  })
+  .set("What Are JavaScript Callbacks?", {
+    url: "https://www.youtube.com/watch?v=c5C8KRf-kbM&feature=youtu.be",
+    criteria: "Object Oriented Code",
+    type: "Video Tutorial",
+    sharer: "Donald Maness"
+  })
+  .set("What is Asynchronous Programming in JavaScript?", {
+    url: "https://www.youtube.com/watch?v=d8XZEgM71GI&feature=youtu.be",
+    criteria: "Object Oriented Code",
+    type: "Video Tutorial",
+    sharer: "Donald Maness"
+  })
+  .set("Interactive Key Codes", {
+    url: "http://keycode.info/",
+    criteria: "Player/Vehicle Movement",
+    type: "App",
+    sharer: "JamesDBartlett3"
+  })
+  .set("KeyboardEvent Value (keyCodes, metaKey, etc)", {
+    url: "https://css-tricks.com/snippets/javascript/javascript-keycodes/",
+    criteria: "Player/Vehicle Movement",
+    type: "Article/Blog",
+    sharer: "Ivan.ProjectCoach [FEND}"
+  })
+  .set("Javascript Char Codes (Key Codes)", {
+    url: "https://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes",
+    criteria: "Player/Vehicle Movement",
+    type: "Article/Blog",
+    sharer: "Ivan.ProjectCoach [FEND}"
+  })
+  .set("Key Codes", {
+    url: "http://blogs.longwin.com.tw/lifetype/key_codes.html",
+    criteria: "Player/Vehicle Movement",
+    type: "Article/Blog",
+    sharer: "Ivan.ProjectCoach [FEND}"
+  })
+  .set("Object-oriented Programming in JavaScript: Made Super Simple | Mosh", {
+    url: "https://www.youtube.com/watch?v=PFmuCDHHpwk",
+    criteria: "Object Oriented Code",
+    type: "Video Tutorial",
+    sharer: "@Sara Krum"
+  })
+  .set("Object Creation in JavaScript", {
+    url: "https://www.youtube.com/playlist?list=PL0zVEGEvSaeHBZFy6Q8731rcwk0Gtuxub",
+    criteria: "Object Oriented Code",
+    type: "Video Tutorial",
+    sharer: "Camille FEND"
+  })
+  .set("JavaScript OOP Crash Course (ES5 & ES6)", {
+    url: "https://www.youtube.com/watch?v=vDJpGenyHaA&app=desktop",
+    criteria: "Object Oriented Code",
+    type: "Video Tutorial",
+    sharer: "Dionne [FEND]"
+  })
+  .set("Eloquent JavaScript Chapter 8: Object-oriented Programming", {
+    url: "http://eloquentjavascript.net/1st_edition/chapter8.html",
+    criteria: "Object Oriented Code",
+    type: "Book",
+    sharer: "Dionne [FEND]"
+  })
+  .set("OOP In JavaScript: What You NEED to Know", {
+    url: "http://javascriptissexy.com/oop-in-javascript-what-you-need-to-know/",
+    criteria: "Object Oriented Code",
+    type: "Article/Blog",
+    sharer: "Dionne [FEND]"
+  })
+  .set("A collision detection function", {
+    url: "https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection",
+    criteria: "Collision",
+    type: "Text Tutorial",
+    sharer: "Tony Boswell [FEND]"
+  });
